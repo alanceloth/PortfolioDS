92c409466a887ab7bba098fbac41872edc11736a
alan.lanceloth@gmail.com <alan.lanceloth@gmail.com>
2023-07-30 14:33:48 +0000

    novo projeto

---
+++ Feature Store - Case IGDB/Untitled Notebook 2023-07-30 11_23_25.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #Feature Store - Case IGDB
+# MAGIC Conseguimos dizer quais empresas tem amiores chances de lançar um game de sucesso nos próximos 12 meses?
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %python
+# MAGIC
